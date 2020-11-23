ce0fd1529bd2f88eb6ddeb87a921dc27b8629944
Passworderror29 <iflamegooglic@gmail.com>
2020-11-23 16:33:53 +0600

    🚧 js functionality added

--- js/main.js
+++ js/main.js
@@ -28,12 +28,14 @@ loginButton.addEventListener('click', () => {
 
 function resizeTrigger(){
   if(window.innerWidth < 697){
-    $('.user').children().remove();
-
+    
+    $('.user').hide();
+    $('.form-modal').show();
   }
   else
   {
-    $('.user').children().add();
+    $('.user').show();
+    $('.form-modal').hide();
   }
 }
 
@@ -49,7 +51,7 @@ function toggleSignup(){
    document.getElementById("login-toggle").style.color="#222";
    document.getElementById("signup-toggle").style.backgroundColor="#57b846";
    document.getElementById("signup-toggle").style.color="#fff";
-   $('#login-form').hide('slow'); 
+   $('#login-form').hide('slow');
    $('#signup-form').show('slow');
 }
 
@@ -58,6 +60,6 @@ function toggleLogin(){
   document.getElementById("login-toggle").style.color="#fff";
   document.getElementById("signup-toggle").style.backgroundColor="#fff";
   document.getElementById("signup-toggle").style.color="#222";
-  $('#signup-form').hide('slow'); 
+  $('#signup-form').hide('slow');
   $('#login-form').show('slow');
 }
