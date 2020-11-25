857b7afe214514075d5688cc8733873282d07951
Passworderror29 <iflamegooglic@gmail.com>
2020-11-25 21:39:24 +0600

    login done

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,5 @@
+// signup functionalities
+
 const signUpform = $('.user_forms-signup')
 const signUpformSmall = $('#signup-form')
 
@@ -6,10 +8,9 @@ signUpform.on('submit',function(event){
     const name = signUpform.find('#name')[0].value;
     const email = signUpform.find('#email')[0].value;
     const password = signUpform.find('#password')[0].value;
-
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        var currentUser = firebase.auth().currentUser;
+        var currentUser = auth.currentUser;
         currentUser.updateProfile({
         displayName: name, //setting up the user name with account display name
         });
@@ -31,9 +32,12 @@ signUpform.on('submit',function(event){
             "showMethod": "fadeIn",
             "hideMethod": "fadeOut"
           }
-         toastr["success"]("you are good to go!", "Successfully signed up")
+         toastr["success"]("you are good to go!", "successfully signed up")
          signUpform.find('input:not(#signUpButton)').val('');
-         $('#login-button').click();
+        //  auth.signOut().then(() => {
+        //     console.log(prevUser+' has been logged out');
+        // })
+        $('#login-button').click();
     })
  });
 
@@ -45,7 +49,7 @@ signUpformSmall.on('submit',function(event){
 
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        var currentUser = firebase.auth().currentUser;
+        var currentUser = auth.currentUser;
         currentUser.updateProfile({
         displayName: name, //setting up the user name with account display name
         });
@@ -69,6 +73,43 @@ signUpformSmall.on('submit',function(event){
           }
          toastr["success"]("you are good to go!", "Successfully signed up")
          signUpformSmall.find('input:not(#signUpButton)').val('');
+         auth.signOut().then(() => {
+             console.log('user has been logged out');
+         })
          toggleLogin();
     })
+    
  });
+
+
+//  login functionalities
+
+const loginForm = $('.user_forms-login');
+loginForm.on('submit', function(e) {
+    e.preventDefault();
+    const email = loginForm.find('#email')[0].value;
+    const password = loginForm.find('#password')[0].value;
+
+    auth.signInWithEmailAndPassword(email, password).then( cred => {
+        console.log(cred.user);
+        window.location.replace("./success.html");
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+          }
+         toastr["info"]("you are logged in!", "hello "+auth.currentUser.displayName)
+    })
+})
