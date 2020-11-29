66bb6b924b8dd7e9d29637f9696210b1f892b3ce
Passworderror29 <iflamegooglic@gmail.com>
2020-11-29 13:07:45 +0600

    ✨ searchbar implemented

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,31 @@
+$(document).ready(function () {
+  $(sliCont).css('width', '0')
+});
 
-    $('[data-toggle="tooltip"]').tooltip()
-  
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+//// slide out search
+var sliBtn = '.search-btn',
+  sliCont = '.search-slide',
+  sliTxt = '.search-slide input[type=text]',
+  sliSpd = 300;
+let searchClicked = false;
+
+$(sliBtn).click(function () {
+  if(!searchClicked) {
+    $(sliCont).animate(
+      { 'width': '46vw' }, sliSpd
+    );
+    $(sliTxt).focus();
+    searchClicked = true;
+  }
+  else {
+    $(sliCont).animate(
+      { 'width': 0 }, sliSpd
+    );
+    searchClicked = false;
+  }
+});
