12582aea78bdbe4c4c900b1badeb360c3c2fad2c
shuvo18103107 <18103107@iubat.edu>
2020-12-27 16:36:10 +0600

    setting page bio prb

--- js/settings.js
+++ js/settings.js
@@ -1,37 +1,63 @@
+$(document).ready(function(){
+  db.collection("users").get()
+  .then(function (querySnapshot) {
+
+      querySnapshot.forEach(function (doc) {
+          if (doc.id == auth.currentUser.email) {
+            $('#userImage').attr("src", `${doc.data().photoURL}`);
+            $('.userName').html(`${doc.data().displayName}`);
+            $('.designation').html(`${doc.data().designation}`);
+          }
+         
+         
+      });
+      $('.loader').fadeOut('slow');
+    
+  })
+
+
+  
 // time js
 function startTime() {
-    let days = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-    ];
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-  
-    m = checkTime(m);
-  
-    var formattedTime = twelveHour(h, m);
-  
-    document.getElementById('time').innerHTML = formattedTime;
-    document.getElementById('date').innerHTML =
-      days[today.getDay()] + ', ' + today.getDate();
-    setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
-    if (i < 10) {
-      i = '0' + i;
-    } // add zero in front of numbers < 10
-    return i;
-  }
-  // twelve hour formatted time
-  function twelveHour(h, m) {
-    var AmOrPm = h >= 12 ? 'PM' : 'AM';
-    h = h % 12 || 12;
-    return h + ':' + m + ' ' + AmOrPm;
-  }
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+
+  m = checkTime(m);
+
+  var formattedTime = twelveHour(h, m);
+
+  document.getElementById('time').innerHTML = formattedTime;
+  document.getElementById('date').innerHTML =
+    days[today.getDay()] + ', ' + today.getDate();
+  setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = '0' + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+// twelve hour formatted time
+function twelveHour(h, m) {
+  var AmOrPm = h >= 12 ? 'PM' : 'AM';
+  h = h % 12 || 12;
+  return h + ':' + m + ' ' + AmOrPm;
+}
+})
+
+
+
+
+
+
   
