4163f648be4bc28cc64e03abb4c59a2a73a9dd84
Passworderror29 <iflamegooglic@gmail.com>
2020-12-29 12:00:28 +0600

    🔥 image resizable done

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,4 @@
-var userBio,userPhoto,userDesignation;
+let userBio,userPhoto,userDesignation, myBlob, convertedImage;
 $(document).ready(function(){
   $('.uploader').fadeOut();
   if(localStorage.getItem("theme") == "dark"){
@@ -24,8 +24,8 @@ $(document).ready(function(){
 });
 
 // drag box js
-var $fileInput = $('.file-input');
-var $droparea = $('.file-drop-area');
+let $fileInput = $('.file-input');
+let $droparea = $('.file-drop-area');
 
 // highlight drag area
 $fileInput.on('dragenter focus click', function() {
@@ -39,12 +39,12 @@ $fileInput.on('dragleave blur drop', function() {
 
 // change inner text
 $fileInput.on('change', function() {
-  var filesCount = $(this)[0].files.length;
-  var $textContainer = $(this).prev();
+  let filesCount = $(this)[0].files.length;
+  let $textContainer = $(this).prev();
 
   if (filesCount === 1) {
     // if single file is selected, show file name
-    var fileName = $(this).val().split('\\').pop();
+    let fileName = $(this).val().split('\\').pop();
     $textContainer.text(fileName);
   } else {
     // otherwise show number of files
@@ -55,30 +55,30 @@ $fileInput.on('change', function() {
 
 })
 
+    function startTime() {
+      let days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ];
+      let today = new Date();
+      let h = today.getHours();
+      let m = today.getMinutes();
 
-function startTime() {
-  let days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
+      m = checkTime(m);
 
-  m = checkTime(m);
+      let formattedTime = twelveHour(h, m);
 
-  var formattedTime = twelveHour(h, m);
+      document.getElementById('time').innerHTML = formattedTime;
+      document.getElementById('date').innerHTML =
+        days[today.getDay()] + ', ' + today.getDate();
+      setTimeout(startTime, 500);
+    }
 
-  document.getElementById('time').innerHTML = formattedTime;
-  document.getElementById('date').innerHTML =
-    days[today.getDay()] + ', ' + today.getDate();
-  setTimeout(startTime, 500);
-}
     function checkTime(i) {
         if (i < 10) {
             i = '0' + i;
@@ -87,42 +87,92 @@ function startTime() {
     }
     // twelve hour formatted time
     function twelveHour(h, m) {
-        var AmOrPm = h >= 12 ? 'PM' : 'AM';
+        let AmOrPm = h >= 12 ? 'PM' : 'AM';
         h = h % 12 || 12;
         return h + ':' + m + ' ' + AmOrPm;
     }
 
-    $('#profilePic').on('change', function () {
-          $('#exampleModalCenter').modal('show')
-          var croppieDemo = $('#exampleModalCenter .modal-body').croppie({
-            enableOrientation: true,
-            viewport: {
-                width: 200,
-                height: 200,
-                type: 'circle' // or 'square'
-            },
-            boundary: {
-                width: 300,
-                height: 300
-            }
-        });
-        var reader = new FileReader();
-        reader.onload = function (f) {
-            croppieDemo.croppie('bind', {
-                url: f.target.result
-            });
-        }
-        reader.readAsDataURL(this.files[0]);
-    });
+    function blobToFile(theBlob, fileName){
+      //A Blob() is almost a File() - it's just missing the two properties below which we will add
+      theBlob.lastModifiedDate = new Date();
+      theBlob.name = fileName;
+      return theBlob;
+    }
 
+// TODO:head
+  let $modal = $('#exampleModalCenter');
+	let image = document.getElementById('sample_image');
+	let cropper;
+
+	$('#profilePic').change(function(event){
+		let files = event.target.files;
+		let done = function(url){
+			image.src = url;
+			$modal.modal('show');
+		};
+
+		if(files && files.length > 0)
+		{
+			let reader = new FileReader();
+      reader.onload = function(e)
+			{
+         //Initiate the JavaScript Image object.
+        var img = new Image();
+        //Set the Base64 string return from FileReader as source.
+        img.src = e.target.result;
+        //Validate the File Height and Width.
+        img.onload = function () {
+          var height = this.height;
+          var width = this.width;
+          if(height / width != 1) {
+            done(reader.result);
+          }
+        };
+			};
+			reader.readAsDataURL(files[0]);
+		}
+	});
+
+	$modal.on('shown.bs.modal', function() {
+		cropper = new Cropper(image, {
+			aspectRatio: 1,
+			viewMode: 2,
+			preview:'#userImage'
+		});
+	}).on('hidden.bs.modal', function(){
+		cropper.destroy();
+   		cropper = null;
+	});
+
+	$('#crop').click(function(){
+		let canvas = cropper.getCroppedCanvas({
+			width:200,
+			height:200
+		});
+
+		canvas.toBlob(function(blob){
+      myBlob = blob;
+			let url = URL.createObjectURL(blob);
+			let reader = new FileReader();
+			reader.readAsDataURL(blob);
+			reader.onloadend = function(){
+          // let base64data = reader.result;
+          $modal.modal('hide');
+          // var renderImage = new Image();
+          // renderImage.src = base64data;
+          // $('#userImage').append(renderImage);
+          // $('#userImage').attr('src', url);
+      };
+		});
+  });
 
     $('.taskForm form').on('submit',function(e){
       e.preventDefault();
-      var currentPass = $('#userPass').val();
-      var newPass = $('#settings-pass').val() == ''? 'oldPass' : $('#settings-pass').val();
-      var displayName = $('#settings-name').val() == ''? auth.currentUser.displayName : $('#settings-name').val();
-      var bioDetails = $('#bioDetails').val() == ''? userBio : $('#bioDetails').val();
-      var profilePic = $('#profilePic')[0].files;
+      let currentPass = $('#userPass').val();
+      let newPass = $('#settings-pass').val() == ''? 'oldPass' : $('#settings-pass').val();
+      let displayName = $('#settings-name').val() == ''? auth.currentUser.displayName : $('#settings-name').val();
+      let bioDetails = $('#bioDetails').val() == ''? userBio : $('#bioDetails').val();
+      let profilePic = $('#profilePic')[0].files;
 
       //console.log(auth);
       // alert(profilePic[0].name);
@@ -131,26 +181,20 @@ function startTime() {
           $('.uploader').fadeIn('slow');
           // profile pic change
           if(profilePic.length > 0){
-            let file = profilePic[0];
-
-            // var basic = $('#demo-basic').croppie({
-            //   enableExif: true,
-            //     viewport: {
-            //         width: 200,
-            //         height: 200,
-            //         type: 'circle'
-            //     }
-            //   });
-            //   basic.croppie('bind', {
-            //     url: 'https://i.imgur.com/xD9rzSt.jpg',
-            //     points: [77, 469, 280, 739]
-            //   });
+            let file;
+            if(myBlob == undefined){
+              file = profilePic[0];
+            }
+            else{
+              file = blobToFile(myBlob, "profilePicture.jpg");
+            }
+            
 
             let storageRef = storage.ref("Users/"+auth.currentUser.email+"/"+'profilePic.jpg');
             let uploadProgress = storageRef.put(file);
 
           uploadProgress.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
-                  var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                   console.log('Upload is ' + progress + '% done');
                   switch (snapshot.state) {
                     case firebase.storage.TaskState.PAUSED: // or 'paused'
